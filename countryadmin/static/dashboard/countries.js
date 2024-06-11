a14e0190be5f9501d5a1bbbc07045eb8f4e64027
ffprivacy <ffprivacy@netcourrier.com>
2024-06-11 16:12:45 +0200

    fix

--- countryadmin/static/dashboard/countries.js
+++ countryadmin/static/dashboard/countries.js
@@ -6,8 +6,8 @@ const countryResourcesDefaults = {
         water: { amount: 200000, renew_rate: 0.01 },
         oil: { amount: 0, renew_rate: 0 },
         gas: { amount: 0, renew_rate: 0 },
-        envEmissions: { amount: 1000000, renew_rate: 0.01 },
-        pm25: { amount: 12, renew_rate: 0.001 },
+        envEmissions: { amount: 1000000, renew_rate: -0.01 },
+        pm25: { amount: 12, renew_rate: -0.001 },
         social: { amount: 50, renew_rate: 0.002 },
         economic: { amount: 250000000, renew_rate: 0.03 }
     },
@@ -18,8 +18,8 @@ const countryResourcesDefaults = {
         water: { amount: 3000000, renew_rate: 0.01 },
         oil: { amount: 500000, renew_rate: 0.005 },
         gas: { amount: 800000, renew_rate: 0.005 },
-        envEmissions: { amount: 5000000, renew_rate: 0.02 },
-        pm25: { amount: 15, renew_rate: 0.002 },
+        envEmissions: { amount: 5000000, renew_rate: -0.02 },
+        pm25: { amount: 15, renew_rate: -0.002 },
         social: { amount: 100, renew_rate: 0.003 },
         economic: { amount: 1800000000, renew_rate: 0.04 }
     }
@@ -31,6 +31,6 @@ document.getElementById('prefill-button').addEventListener('click', function() {
 
     for(let metric of Processes.metricsGetIdsList()) {
         document.getElementById(`country-resources-${metric}-amount`).value = defaults[metric].amount;
-        document.getElementById(`country-resources-${metric}-renew-rate`).renew_rate = defaults[metric].renew_rate;
+        document.getElementById(`country-resources-${metric}-renew-rate`).value = defaults[metric].renew_rate;
     }
 });
