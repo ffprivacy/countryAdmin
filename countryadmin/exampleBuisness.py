557f2a8bacbd743bc0537dc4586cfc2f08f1f1f3
ffprivacy <ffprivacy@netcourrier.com>
2024-06-13 16:30:27 +0200

    buisnesses exposes one or many processes

---
+++ countryadmin/exampleBuisness.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+import threading
+from countryadmin.countryAdmin import run_app
+import os
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session, send_file
+import socket
+
+app = Flask(__name__)
+@app.route('/api/processes', methods=['GET'])
+def get_processes():
+    return [
+        {
+            "amount": 0,
+            "comments": [],
+            "composition": [],
+            "id": 1,
+            "like_count": 0,
+            "metrics": {
+            "input": {
+                "economic": 10,
+                "envEmissions": 0,
+                "gas": 0,
+                "ground": 0,
+                "human": 0,
+                "oil": 0,
+                "ores": 10,
+                "pm25": 0,
+                "social": 0,
+                "water": 0
+            },
+            "output": {
+                "economic": 0,
+                "envEmissions": 50,
+                "gas": 0,
+                "ground": 0,
+                "human": 0,
+                "oil": 0,
+                "ores": 0,
+                "pm25": 0,
+                "social": 0,
+                "water": 0
+            }
+            },
+            "selected": False,
+            "tags": [],
+            "title": "Faire une voiture thermique"
+        },
+    ]
+
+if __name__ == '__main__':
+    app.run(debug=True, port=6000, use_reloader=True)
