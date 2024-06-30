5d0ce4d89205b72605ae02da255aa3c4abd55f28
ffprivacy <ffprivacy@netcourrier.com>
2024-06-30 22:54:41 +0200

    add some

--- areaadmin/static/dashboard/dashboard.js
+++ areaadmin/static/dashboard/dashboard.js
@@ -38,139 +38,6 @@ function updateRadarChart(economic, envEmissions, social) {
 		}
 	});
 }
-
-function setScenario(scenario) {
-	let economic, social, envEmissions;
-	if (scenario === 'capitalism') {
-		economic = 1000;
-		social = 0;
-		envEmissions = 1000;
-	} else if (scenario === 'hell') {
-		economic = 1000;
-		social = 0;
-		envEmissions = 0;
-	} else if (scenario === 'diamond') {
-		economic = 1000;
-		social = 1000;
-		envEmissions = 0;
-	}
-
-	document.getElementById('economic-goals').value = economic;
-	document.getElementById('social-goals').value = social;
-	document.getElementById('governance-goals-env-emissions').value = envEmissions;
-}
-
-function selectProcesses() {
-	const processList = document.getElementById('process-list');
-
-	const economicGoal = parseInt(document.getElementById('economic-goals').value, 10);
-	const govGoalEnvEmissions = parseInt(document.getElementById('governance-goals-env-emissions').value, 10);
-	const socialGoal = parseInt(document.getElementById('social-goals').value, 10);
-
-	const allProcesses = [...processList.getElementsByTagName('li')].map(li => {
-		const form = li.querySelector('form');
-		const checkbox = form.querySelector('input[name="selected"]');
-		const id = parseInt(form.querySelector('input[name="id"]').value);
-		const process_id = parseInt(li.getAttribute("process-id"));
-		const metrics = JSON.parse(li.getAttribute("metrics"));
-		const amount = parseInt(li.getAttribute("process-amount"));
-		const composition = addProcessGetCompositionData();
-		return { id, process_id, metrics, selected: checkbox.checked, amount, composition, form };
-	});
-
-	// Unselect all processes
-	allProcesses.forEach(process => {
-		process.selected = false;
-		process.form.querySelector('input[name="selected"]').checked = false;
-	});
-
-	// Group processes by process_id
-	const processMap = new Map();
-	allProcesses.forEach(process => {
-		if (!processMap.has(process.process_id)) {
-			processMap.set(process.process_id, []);
-		}
-		processMap.get(process.process_id).push(process);
-	});
-
-	/**
-	 *  Procédure pour choisir les process à utiliser parmis les autres.
-	 * 
-	 *  Remplir les objectifs en emissions en faisant en sorte que la charge de travail (on parle en h) soit répartie.
-	 *  Tout en tenant compte des différences de capacité des travailleurs (formation, compétences, nature).
-	 *  Les objectifs en emissions peuvent être une contrainte si elles amènent à plus de travail de la part des travailleurs et c'est ce plus qu'il faut répartir,
-	 *   pour rester socialement juste.
-	 *  Mais dans certains cas la modification est transparente (thermique -> electrique) (decarbonation de l'electricité) (des ampoules qui consomment moins)
-	 *  Résoudre toutes les modifications transparentes pour l'utilisateur et si le producteur n'obtiens pas trop de contraintes (reste viable economiquement et socialement).
-	 *  Un exemple typique de modidification non transparente et couteuse aux utilisateurs, le composte (un vrai), c'est à dire une modification du process "Jeter un déchet vert".
-	 *   un autre exemple de process "Se débarrasser d'un inutile". Il y a le tri des déchets, une modification du process coûteuse aux utilisateurs. Il y a jeter dans la nature.
-	 *    pour ce dernier une amélioration du process sans impacter les utilisateurs, trier mécaniquement grace à l'ia les déchets peut importe ce qui arrive.
-	 * 
-	 *  Remplir les objectifs en économie n'a pas forcément besoin d'être équilibré (service public en négatif, investissement en positif)
-	 *  On peut adopter contraintes min sur certains points critiques de l'économie, mais le social baisse.
-	 *  Pour que le social tienne au nom de la cohésion sociale, il n'en faut pas beaucoup, sinon la metrique sociale baisse de trop et il faut
-	 *  redistribution en fonction de la charge de travail.
-	 *  Du fait du jeu, il y a de la rareté des biens (ne reflète pas la quantité de travail donné), c'est un biais humain.
-	 *  
-	 */
-	let totalEconomic = 0;
-	let selectedGovEnvEmissions = 0;
-	let totalSocial = 0;
-
-	// Calculate the distance between each process and the goals
-	const calculateDistance = (allProcesses, process, goalEconomic, goalEnvEmissions, goalSocial) => {
-		const distanceEconomic = Math.abs(process.amount * Processes.retrieveMetric(allProcesses, process, "economic") - goalEconomic);
-		const distanceEnvEmissions = Math.abs(process.amount * Processes.retrieveMetric(allProcesses, process, "envEmissions") - goalEnvEmissions);
-		const distanceSocial = Math.abs(process.amount * Processes.retrieveMetric(allProcesses, process, "social") - goalSocial);
-		return distanceEconomic + distanceEnvEmissions + distanceSocial;
-	};
-
-	// Select processes based on goals
-	/**
-	 * Les processus qui peuvent être permutables devraint être recherchés automatiquement.
-	 * On pourrait dans un premier temps (comme c'était avant le cas utiliser un process_id ou group de processus interchangeable) (qui ont strictement les mêmes but du point de vue utilisateur)
-	 * Ou alors procéder en produits/consommable: des processus au vu des contraintes de la gouvernance produisent les même choses et
-	 * qui consomment à peu près les mêmes resources voir moins dans l'idéal peuvent être permutés.
-	 * Par exemple sur la voiture électrique peut être qu'il y a un peu plus potentiellement de travail à la production,
-	 * mais moins de travail dans la recharge (transport des hydrocarb, personnel, etc).
-	 */
-	const selectedProcessIds = new Set();
-	processMap.forEach(processes => {
-		let minDistance = Infinity;
-		let closestProcess = null;
-		for (const process of processes) {
-			if (!selectedProcessIds.has(process.process_id)) {
-				const distance = calculateDistance(allProcesses, process, economicGoal - totalEconomic, govGoalEnvEmissions - selectedGovEnvEmissions, socialGoal - totalSocial);
-				if (distance < minDistance) {
-					minDistance = distance;
-					closestProcess = process;
-				}
-			}
-		}
-		if (closestProcess) {
-			closestProcess.selected = true;
-			selectedProcessIds.add(closestProcess.process_id);
-			totalEconomic += Processes.retrieveMetric(allProcesses, closestProcess, "economic") * closestProcess.amount;
-			selectedGovEnvEmissions += Processes.retrieveMetric(allProcesses, closestProcess, "envEmissions") * closestProcess.amount;
-			totalSocial += Processes.retrieveMetric(allProcesses, closestProcess, "social") * closestProcess.amount;
-		}
-	});
-
-	const formData = new FormData();
-	allProcesses.forEach(process => {
-		formData.append('id[]', process.id);
-		formData.append('selected[]', process.selected);
-	});
-
-	fetchAreaAPI('/select_process', {
-		method: 'POST',
-		body: formData
-	})
-		.then(() => dashboardRefresh())
-		.catch(function (e) {
-			console.warn(e);
-		});
-}
 function dashboardRefresh() {
     return fetchAreaAPI('/processes')
         .then(async function (data) {
@@ -428,7 +295,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 	updateRadarChart(0, 0, 0);
 
-	document.getElementById("btn-adjust").addEventListener("click", selectProcesses);
+	document.getElementById("btn-adjust").addEventListener("click", governanceBuildScenario);
 
 	// Fetch processes when the page loads
 	dashboardRefresh();
